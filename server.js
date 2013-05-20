2029326dd856cc5e941b3b10eecbb28b0bf79cd0
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2013-05-21 00:32:45 +0200

    Don't crash if itemParam is not a valid ObjectID.

--- server.js
+++ server.js
@@ -25,19 +25,25 @@
  // Parse JSON.
     app.use(express.json());
 
-    app.get("/:collection/:item", function (req, res) {
+    app.get("/:collection/:item", function (req, res, next) {
         var collectionParam, itemParam, client;
         collectionParam = req.params.collection;
         itemParam = req.params.item;
         client = new mongo.Db("test", new mongo.Server("127.0.0.1", 27017), {w: "majority"});
         client.open(function (err, pDb) {
             pDb.collection(collectionParam, function (err, pCollection) {
-                pCollection.findOne({
-                    _id: new mongo.ObjectID(itemParam)
-                }, function (err, item) {
-                    res.send(item);
-                    pDb.close();
-                });
+                var id;
+                try {
+                    id = new mongo.ObjectID(itemParam);
+                    pCollection.findOne({
+                        _id: id
+                    }, function (err, item) {
+                        res.send(item);
+                        pDb.close();
+                    });
+                } catch (e) {
+                    next(e);
+                }
             });
         });
     });
@@ -57,21 +63,31 @@
         });
     });
 
-    app.delete("/:collection/:item", function (req, res) {
+    app.delete("/:collection/:item", function (req, res, next) {
         var collectionParam, itemParam, client;
         collectionParam = req.params.collection;
         itemParam = req.params.item;
         client = new mongo.Db("test", new mongo.Server("127.0.0.1", 27017), {w: "majority"});
         client.open(function (err, pDb) {
             pDb.collection(collectionParam, function (err, pCollection) {
-                pCollection.remove({
-                    _id: new mongo.ObjectID(itemParam)
-                }, function () {
-                    res.send(200, "Deleted");
-                    pDb.close();
-                });
+                var id;
+                try {
+                    id = new mongo.ObjectID(itemParam);
+                    pCollection.remove({
+                        _id: new mongo.ObjectID(itemParam)
+                    }, function () {
+                        res.send(200, "Deleted");
+                        pDb.close();
+                    });
+                } catch (e) {
+                    next(e);
+                }
             });
         });
+    });
+
+    app.use(function(err, req, res, next){
+        res.send(500, "Something broke!");
     });
 
     app.listen(1337);
