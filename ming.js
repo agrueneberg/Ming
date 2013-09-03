c6e10e7accf93cc1db646d27b76c5e76fbf1d0b5
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2013-09-03 09:20:00 -0500

    Improve error routing.

--- ming.js
+++ ming.js
@@ -88,14 +88,18 @@
             namesOnly: true
         }, function (err, collections) {
             var names;
-            names = collections.map(function (collection) {
-             // Strip database name.
-                return collection.substring(req.connectionString.path.length);
-            });
-            res.send({
-                collections: names
-            });
-            req.db.close();
+            if (err !== null) {
+                next(err);
+            } else {
+                names = collections.map(function (collection) {
+                 // Strip database name.
+                    return collection.substring(req.connectionString.path.length);
+                });
+                res.send({
+                    collections: names
+                });
+                req.db.close();
+            }
         });
     });
 
@@ -103,10 +107,18 @@
         var collectionParam;
         collectionParam = req.params.collection;
         req.db.collection(collectionParam, function (err, collection) {
-            collection.find().count(function (err, count) {
-                res.send({count: count});
-                req.db.close();
-            });
+            if (err !== null) {
+                next(err);
+            } else {
+                collection.find().count(function (err, count) {
+                    if (err !== null) {
+                        next(err);
+                    } else {
+                        res.send({count: count});
+                        req.db.close();
+                    }
+                });
+            }
         });
     });
 
@@ -116,42 +128,54 @@
         fileParam = req.params.file;
         req.db.collection(prefixParam + ".files", function (err, collection) {
             var id;
-            try {
-                id = new mongo.ObjectID(fileParam);
-             // Get metadata first.
-                collection.findOne({
-                    _id: id
-                }, function (err, document) {
-                    var grid;
-                    if (document === null) {
-                     // Route to catch-all.
-                        next();
-                    } else {
-                        if (req.query.hasOwnProperty("binary") === true && req.query.binary === "true") {
-                            grid = new mongo.Grid(req.db, prefixParam);
-                            grid.get(id, function (err, file) {
-                                var contentType;
-                                if (document.contentType) {
-                                    contentType = document.contentType;
-                                } else if (document.filename) {
-                                 // Guess file type from file extension.
-                                    contentType = document.filename.match(/(\.\w+)$/)[1];
-                                } else {
-                                    contentType = "application/octet-stream";
-                                }
-                                res.type(contentType);
-                                res.send(file);
-                                req.db.close();
-                            });
+            if (err !== null) {
+                next(err);
+            } else {
+                try {
+                    id = new mongo.ObjectID(fileParam);
+                 // Get metadata first.
+                    collection.findOne({
+                        _id: id
+                    }, function (err, document) {
+                        var grid;
+                        if (err !== null) {
+                            next(err);
                         } else {
-                            res.send(document);
-                            req.db.close();
+                            if (document === null) {
+                             // Route to catch-all.
+                                next();
+                            } else {
+                                if (req.query.hasOwnProperty("binary") === true && req.query.binary === "true") {
+                                    grid = new mongo.Grid(req.db, prefixParam);
+                                    grid.get(id, function (err, file) {
+                                        var contentType;
+                                        if (err !== null) {
+                                            next(err);
+                                        } else {
+                                            if (document.contentType) {
+                                                contentType = document.contentType;
+                                            } else if (document.filename) {
+                                             // Guess file type from file extension.
+                                                contentType = document.filename.match(/(\.\w+)$/)[1];
+                                            } else {
+                                                contentType = "application/octet-stream";
+                                            }
+                                            res.type(contentType);
+                                            res.send(file);
+                                            req.db.close();
+                                        }
+                                    });
+                                } else {
+                                    res.send(document);
+                                    req.db.close();
+                                }
+                            }
                         }
-                    }
-                });
-            } catch (e) {
-             // Route to catch-all.
-                next();
+                    });
+                } catch (e) {
+                 // Route to catch-all.
+                    next();
+                }
             }
         });
     });
@@ -162,22 +186,30 @@
         documentParam = req.params.document;
         req.db.collection(collectionParam, function (err, collection) {
             var id;
-            try {
-                id = new mongo.ObjectID(documentParam);
-                collection.findOne({
-                    _id: id
-                }, function (err, document) {
-                    if (document === null) {
-                     // Route to catch-all.
-                        next();
-                    } else {
-                        res.send(document);
-                    }
-                    req.db.close();
-                });
-            } catch (e) {
-             // Route to catch-all.
-                next();
+            if (err !== null) {
+                next(err);
+            } else {
+                try {
+                    id = new mongo.ObjectID(documentParam);
+                    collection.findOne({
+                        _id: id
+                    }, function (err, document) {
+                        if (err !== null) {
+                            next(err);
+                        } else {
+                            if (document === null) {
+                             // Route to catch-all.
+                                next();
+                            } else {
+                                res.send(document);
+                                req.db.close();
+                            }
+                        }
+                    });
+                } catch (e) {
+                 // Route to catch-all.
+                    next();
+                }
             }
         });
     });
@@ -189,26 +221,34 @@
         fieldParam = req.params.field;
         req.db.collection(collectionParam, function (err, collection) {
             var id, fields;
-            try {
-                id = new mongo.ObjectID(documentParam);
-                fields = {};
-                fields[fieldParam] = 1;
-                collection.findOne({
-                    _id: id
-                }, {
-                    fields: fields
-                }, function (err, document) {
-                    if (document === null || document.hasOwnProperty(fieldParam) === false) {
-                     // Route to catch-all.
-                        next();
-                    } else {
-                        res.send(document[fieldParam]);
-                    }
-                    req.db.close();
-                });
-            } catch (e) {
-             // Route to catch-all.
-                next();
+            if (err !== null) {
+                next(err);
+            } else {
+                try {
+                    id = new mongo.ObjectID(documentParam);
+                    fields = {};
+                    fields[fieldParam] = 1;
+                    collection.findOne({
+                        _id: id
+                    }, {
+                        fields: fields
+                    }, function (err, document) {
+                        if (err !== null) {
+                            next(err);
+                        } else {
+                            if (document === null || document.hasOwnProperty(fieldParam) === false) {
+                             // Route to catch-all.
+                                next();
+                            } else {
+                                res.send(document[fieldParam]);
+                                req.db.close();
+                            }
+                        }
+                    });
+                } catch (e) {
+                 // Route to catch-all.
+                    next();
+                }
             }
         });
     });
@@ -218,20 +258,28 @@
         collectionParam = req.params.collection;
         req.db.collection(collectionParam, function (err, collection) {
             var options;
-            options = {};
-            if (req.query.limit) {
-                options.limit = req.query.limit;
+            if (err !== null) {
+                next(err);
+            } else {
+                options = {};
+                if (req.query.limit) {
+                    options.limit = req.query.limit;
+                }
+                if (req.query.skip) {
+                    options.skip = req.query.skip;
+                }
+                if (req.query.sort) {
+                    options.sort = req.query.sort;
+                }
+                collection.find(req.body, options).toArray(function (err, documents) {
+                    if (err !== null) {
+                        next(err);
+                    } else {
+                        res.send(documents);
+                        req.db.close();
+                    }
+                });
             }
-            if (req.query.skip) {
-                options.skip = req.query.skip;
-            }
-            if (req.query.sort) {
-                options.sort = req.query.sort;
-            }
-            collection.find(req.body, options).toArray(function (err, documents) {
-                res.send(documents);
-                req.db.close();
-            });
         });
     });
 
@@ -243,9 +291,13 @@
         grid.put(req.body, {
             content_type: contentType
         }, function (err, document) {
-            res.location(prefixParam + ".files/" + document._id.toHexString());
-            res.send(201, "Created");
-            req.db.close();
+            if (err !== null) {
+                next(err);
+            } else {
+                res.location(prefixParam + ".files/" + document._id.toHexString());
+                res.send(201, "Created");
+                req.db.close();
+            }
         });
     });
 
@@ -254,11 +306,19 @@
         collectionParam = req.params.collection;
         payload = req.body;
         req.db.collection(collectionParam, function (err, collection) {
-            collection.insert(payload, function (err, document) {
-                res.location(collectionParam + "/" + document[0]._id.toHexString());
-                res.send(201, "Created");
-                req.db.close();
-            });
+            if (err !== null) {
+                next(err);
+            } else {
+                collection.insert(payload, function (err, document) {
+                    if (err !== null) {
+                        next(err);
+                    } else {
+                        res.location(collectionParam + "/" + document[0]._id.toHexString());
+                        res.send(201, "Created");
+                        req.db.close();
+                    }
+                });
+            }
         });
     });
 
@@ -268,17 +328,25 @@
         documentParam = req.params.document;
         req.db.collection(collectionParam, function (err, collection) {
             var id;
-            try {
-                id = new mongo.ObjectID(documentParam);
-                collection.update({
-                    _id: id
-                }, req.body, function (err) {
-                    res.send(204, "No Content");
-                    req.db.close();
-                });
-            } catch (e) {
-             // Route to catch-all.
-                next();
+            if (err !== null) {
+                next(err);
+            } else {
+                try {
+                    id = new mongo.ObjectID(documentParam);
+                    collection.update({
+                        _id: id
+                    }, req.body, function (err) {
+                        if (err !== null) {
+                            next(err);
+                        } else {
+                            res.send(204, "No Content");
+                            req.db.close();
+                        }
+                    });
+                } catch (e) {
+                 // Route to catch-all.
+                    next();
+                }
             }
         });
     });
@@ -291,9 +359,13 @@
         try {
             id = new mongo.ObjectID(fileParam);
             grid.delete(id, function (err, flag) {
-             // TODO: Detect if file was actually deleted.
-                res.send(200, "OK");
-                req.db.close();
+                if (err !== null) {
+                    next(err);
+                } else {
+                 // TODO: Detect if file was actually deleted.
+                    res.send(200, "OK");
+                    req.db.close();
+                }
             });
         } catch (e) {
          // Route to catch-all.
@@ -307,22 +379,30 @@
         documentParam = req.params.document;
         req.db.collection(collectionParam, function (err, collection) {
             var id;
-            try {
-                id = new mongo.ObjectID(documentParam);
-                collection.remove({
-                    _id: id
-                }, function (err, num) {
-                    if (num === 0) {
-                     // Route to catch-all.
-                        next();
-                    } else {
-                        res.send(200, "Deleted");
-                    }
-                    req.db.close();
-                });
-            } catch (e) {
-             // Route to catch-all.
-                next();
+            if (err !== null) {
+                next(err);
+            } else {
+                try {
+                    id = new mongo.ObjectID(documentParam);
+                    collection.remove({
+                        _id: id
+                    }, function (err, num) {
+                        if (err !== null) {
+                            next(err);
+                        } else {
+                            if (num === 0) {
+                             // Route to catch-all.
+                                next();
+                            } else {
+                                res.send(200, "Deleted");
+                                req.db.close();
+                            }
+                        }
+                    });
+                } catch (e) {
+                 // Route to catch-all.
+                    next();
+                }
             }
         });
     });
